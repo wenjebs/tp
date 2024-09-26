354f4a3ea7034b2480a02f63e53c0ca608afb2d6
wenjebs <wenjieb2@gmail.com>
2024-09-27 00:36:34 +0800

    Final final final

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -1,20 +1,22 @@
 package seedu.address.logic.parser;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.RemarkCommand;
-import seedu.address.model.person.Remark;
-
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.person.Remark;
+
+
 public class RemarkCommandParserTest {
-    private RemarkCommandParser parser = new RemarkCommandParser();
     private final String nonEmptyRemark = "Some remark.";
+    private RemarkCommandParser parser = new RemarkCommandParser();
+
     @Test
     public void parse_indexSpecified_success() {
         // have remark
